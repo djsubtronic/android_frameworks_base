618363b8a3903e7a8c229da630959f208774d434
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2012-03-01 18:46:45 -0800

    Camera: Framework changes to support Power Modes
    Change-Id: Ie74171476b1db1502695d7091009372e2d6a9054
    CRs-Fixed: 349724
    
    Conflicts:
    
    	api/current.txt
    	core/java/android/hardware/Camera.java

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -578,6 +578,7 @@ public:
     // Example value: "true" or "false". Read only.
     static const char KEY_VIDEO_SNAPSHOT_SUPPORTED[];
     static const char KEY_FULL_VIDEO_SNAP_SUPPORTED[];
+    static const char KEY_POWER_MODE_SUPPORTED[];
 
 #ifdef QCOM_HARDWARE
     static const char KEY_LENSSHADE[] ;
@@ -614,6 +615,9 @@ public:
 #ifdef QCOM_HARDWARE
     static const char KEY_MEMORY_COLOR_ENHANCEMENT[];
     static const char KEY_SUPPORTED_MEM_COLOR_ENHANCE_MODES[];
+
+
+    static const char KEY_POWER_MODE[];
 
     static const char KEY_ZSL[];
     static const char KEY_SUPPORTED_ZSL_MODES[];
@@ -901,6 +905,10 @@ public:
     static const char AE_BRACKET_HDR_OFF[];
     static const char AE_BRACKET_HDR[];
     static const char AE_BRACKET[];
+
+    // Values for Power mode settings.
+    static const char LOW_POWER[];
+    static const char NORMAL_POWER[];
 
     // Values for HFR settings.
     static const char VIDEO_HFR_OFF[];
